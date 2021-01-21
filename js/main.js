20bbcddcc99e4031dd1b037f40f39dc1c7cd10f0
doramakh <anvarovna28@gmail.com>
2021-01-21 17:45:44 +0500

    display category names

--- js/main.js
+++ js/main.js
@@ -22,14 +22,40 @@ var createElement = function (element, elementClass, text) {
 
 // variable for user input (title) source
 var titleRegex = '';
-
-// DOM
+var categories = [];
 
 // create an array of top 100 movies
 var topHundrendMovies = movies.slice().sort((a,b) => b.imdbRating - a.imdbRating).slice(0, 100);
 
+
+// DOM
+
 var elMoviesList = $_('.movies-list');
 var elMovieCardTemplate = $_('.movie-card-template').content;
+
+var elSearchForom = $_('.search-form');
+var elTitleInput = $_('.title-input');
+var elYearInput = $_('.year-input');
+var elCategorySelect = $_('.category-select');
+var elratingSelect = $_('.rating-select');
+
+// Push category names from movie object to categories array
+for (var movie of movies) {
+  for (var category of movie.categories) {
+    if (!categories.includes(category)){
+      categories.push(category);
+    };
+  };
+};
+
+// Display categories in html categories select
+for (var category of categories) {
+  var categoryOption = document.createElement('option');
+  categoryOption.textContent = category;
+  categoryOption.value = category;
+
+  elCategorySelect.appendChild(categoryOption);
+} 
 
 // FUNCTIONS 
 
@@ -63,6 +89,10 @@ var displayMovies = movies => {
   elMoviesList.appendChild(elMovieFragment);
 }
 
-// Display to 100 movies 
-
+// Display top 100 movies 
 displayMovies(topHundrendMovies);
+
+elSearchForom.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+})
